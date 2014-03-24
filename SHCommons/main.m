37bfddd70969d987b27219fc48625cec2a24c594
Shan Ul Haq <g@grevolution.me>
2014-03-24 19:41:28 +0800

    Initial Commit

---
+++ SHCommons/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SHCommons
+//
+//  Created by SHAN UL HAQ on 24/3/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+  }
+}
